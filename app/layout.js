948027ea5589edb44916d6997291f299d3d29115
ziyodyakubov <ziyodyakubov23@gmail.com>
2024-12-20 16:20:18 +0500

    folder is already ready

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Real Estate",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
